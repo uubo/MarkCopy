a993178ffe117f702a75e93b3fe7b605b33d8cde
Vlad Agiyevich <sky-mart@hotmail.com>
2015-04-19 17:38:51 +0300

    First page

--- facemash/faces/views.py
+++ facemash/faces/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 
 def compare(request):
-    return HttpResponse("Here will be a page with comparison")
+    template = loader.get_template('faces/compare.html')
+    context = RequestContext(request, {
+        'url1': "http://risovach.ru/upload/2013/06/mem/ya-tvoi-dom-truba-shatal_21385844_orig_.jpg",
+        'url2': "http://troll-face.ru/static/mememaker/f/c/24817-vasya-afrika.jpg",
+    })
+    return HttpResponse(template.render(context))
